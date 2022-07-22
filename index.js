198218dd3fe46445942e3a6325b239649778a9d5
Esmeralda Romero <esmeralda.r.a@hotmail.com>
2022-07-22 12:14:53 -0400

    init package, install inquirer, layout of README, start prompt

---
+++ index.js
@@ -0,0 +1,86 @@
+const {prompt} = require('inquirer');
+
+
+const generateREADME = ({title, description, installation, usage, contribution, test, licence, github, email }) =>
+` #${title}
+
+##Description:
+${description}
+
+##Table of Contents
+
+*[]
+*[]
+*[]
+*[]
+
+
+###Installation:
+${installation}
+
+###Usage:
+${usage}
+
+###License
+${licence}
+
+###Contributing:
+${contribution}
+
+###Tests:
+${test}
+
+###Questions
+[GitHub](${github})
+You can reach me at ${email}`
+
+
+
+prompt([
+    {
+      type: 'input',
+      message: 'Title:',
+      name: 'title',
+    },
+    {
+      type: 'input',
+      message: 'Description:',
+      name: 'description',
+    },
+    {
+      type: 'input',
+      message: 'Installation:',
+      name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'Usage:',
+        name: 'usage',
+    },
+    {
+        type: 'list',
+        message: 'License:',
+        name: 'license',
+        choices: ['']
+    },
+    {
+        type: 'input',
+        message: 'Contributing:',
+        name: 'contributing',
+    },
+    {
+        type: 'input',
+        message: 'Tests:',
+        name: 'tests',
+    },   
+    {
+        type: 'input',
+        message: 'Enter your GitHub Username',
+        name: 'github',
+    },
+    {
+        type: 'input',
+        message: 'Enter your Email',
+        name: 'email',
+    },
+  ])
